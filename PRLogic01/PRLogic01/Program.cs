029d23a50d88208449d9aec1c6d9b949dca32d3e
M Rido Kurniawan <mrkawan77@gmail.com>
2019-01-16 00:48:48 +0700

    Latihan Solusi PR Logic 1

---
+++ PRLogic01/PRLogic01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRLogic01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Soal01 s1 = new Soal01();
+            Soal02 s2 = new Soal02();
+            Soal03 s3 = new Soal03();
+            Soal04 s4 = new Soal04();
+            Soal05 s5 = new Soal05();
+        }
+    }
+}
---
+++ PRLogic01/PRLogic01/Soal04.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRLogic01
+{
+    class Soal04
+    {
+        public Soal04()
+        {
+            Console.Write("Masukkan Nilai n :");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int b = 0; b < n; b++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    if (n % 2 == 1)
+                    {
+                        if (b == k || b + k == n - 1 || b == n / 2 || k == n / 2)
+                            Console.Write("*\t");
+                        else
+                            Console.Write("\t");
+                    }
+                    else
+                    {
+                        int c = n / 2 - 1;
+                        if (b == k || b + k == n - 1 || b == n / 2 || k == n / 2 || b == c || k == c)
+                            Console.Write("*\t");
+                        else
+                            Console.Write("\t");
+                    }
+                }
+
+                Console.Write("\n\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
